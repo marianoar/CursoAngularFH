14b996ca02440a4561c0c405420216d99c192f42
Mariano <arias.mariano.ar@gmail.com>
2024-10-07 18:09:11 -0300

    debounce, se envian los request cuando el usuario deja de escribir

--- 04-countryApp/src/app/shared/components/search-box/search-box.component.ts
+++ 04-countryApp/src/app/shared/components/search-box/search-box.component.ts
@@ -1,19 +1,40 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit{
+
+  private debouncer: Subject<string> = new Subject<string>(); //observable
 
   @Input()
   public placeholder: string = "";
 
   @Output()
   public onValue = new EventEmitter<string>();
+  
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    // el debounceTimer funciona como un delay, hasta que el observable deja d emitir valores por un seg
+    this.debouncer
+    .pipe(
+      debounceTime(750)
+    )
+    .subscribe(value=>{
+      this.onDebounce.emit(value);
+    })
+  }
 
   emitValue(value:string):void{
     this.onValue.emit(value);
+  }
+
+  onKeyPress(search : string){
+    this.debouncer.next(search);
   }
 }
