d1e3deec150904c7648676f402f3192e2645632b
Mariano <arias.mariano.ar@gmail.com>
2024-12-18 16:09:31 -0300

    reactive forms continuacion

--- 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit{
 
+  public myForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){}
+  
+  ngOnInit(){
+    this.myForm = this.fb.group({
+      name:['', [Validators.required, Validators.minLength(3)]],
+      price:[0, [Validators.required, Validators.min(0)]],
+      inStorage:[0, [Validators.required, Validators.min(0)]]
+    })
+  }
+
+  onSave():void{
+    console.log(this.myForm.value);
+  }
 }
