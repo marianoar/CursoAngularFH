455c73f301b1280720fb46429259f1f748ea5345
Mariano <arias.mariano.ar@gmail.com>
2024-12-27 12:05:26 -0300

    formularios dinamicos y validaciones

--- 07-formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -8,12 +8,12 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class DynamicPageComponent implements OnInit{
 
   public myForm!: FormGroup;
-  constructor(private fb: FormBuilder){
 
-  }
+  constructor(private fb: FormBuilder){  }
+
   ngOnInit(): void {
     this.myForm = this.fb.group({
-      nam:['',[Validators.required, Validators.minLength(3)]],
+      name:['',[Validators.required, Validators.minLength(3)]],
       favoriteGames: this.fb.array([
         ['Prince of Persian', Validators.required],
         ['Pacman', Validators.required]
@@ -29,11 +29,15 @@ export class DynamicPageComponent implements OnInit{
     return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
   }
 
+  isValidFieldInArray(formArray: FormArray, i: number){
+    return formArray.controls[i].errors && formArray.controls[i].touched;
+  }
+
   getFieldError(field: string): string | null{
 
     if(!this.myForm.controls[field]) return '';
-    const errors: any = this.myForm.controls[field] || {};
-    console.log(errors);
+    const errors: any = this.myForm.controls[field].errors || {};
+
     for (const key of Object.keys(errors)){
 
       switch(key){
@@ -46,17 +50,16 @@ export class DynamicPageComponent implements OnInit{
     return null;
   }
 
-  isValidFieldInArray(formArray: FormArray, i: number){
-    return formArray.controls[i].errors && formArray.controls[i].touched;
-  }
+
 
   onSubmit():void{
     console.log(this.myForm?.value);
 
-    if(this.myForm?.invalid){
+    if(this.myForm.invalid){
       this.myForm.markAllAsTouched();
       return;
     }
+    this.myForm.reset();
   }
 
 }
