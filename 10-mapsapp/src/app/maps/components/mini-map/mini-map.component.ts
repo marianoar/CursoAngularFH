d3085218dbd339077111951fafa2f87d98602789
Mariano <arias.mariano.ar@gmail.com>
2025-02-11 11:23:04 -0300

    Section 21: Mapas con Mapbox and Angular

--- 10-mapsapp/src/app/maps/components/mini-map/mini-map.component.ts
+++ 10-mapsapp/src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrl: './mini-map.component.css'
 })
-export class MiniMapComponent {
+
+export class MiniMapComponent implements AfterViewInit{
+  
+  @Input()
+  lngLat?: [number,number];
+
+  @ViewChild('map')
+  divMap?: ElementRef;
+  
+  ngAfterViewInit(): void {
+    if(!this.divMap?.nativeElement)
+      throw "Map not found";
+
+    if(!this.lngLat)
+      throw "Lng Lat can't be null";
+
+    const map = new Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.lngLat,
+      zoom: 15,
+      interactive:false
+    });
+
+    new Marker().setLngLat(this.lngLat).addTo(map);
+  }
+
 
 }
--- 10-mapsapp/src/app/maps/pages/markers-page/markers-page.component.ts
+++ 10-mapsapp/src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,6 +1,17 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { LngLat, Map, Marker } from 'mapbox-gl';
 
+//estas interfaces debe ir en arcvhivos independientes
+interface MarkerAndColor {
+  // hace esto porque la propiedad color es privada e innaccesible en el obj Marker
+  color: string;
+  marker: Marker;
+}
+interface PlainMarker {
+  color: string;
+  lngLat: number[];
+}
+
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrl: './markers-page.component.css'
@@ -13,6 +24,7 @@ export class MarkersPageComponent {
   public zoom: number = 15;
   public map?: Map;
   public currentLngLat: LngLat = new LngLat(-56.208239209291136, -34.90750655115182);
+  public markers: MarkerAndColor[] = [];
 
   ngAfterViewInit(): void {
 
@@ -28,24 +40,71 @@ export class MarkersPageComponent {
       zoom: this.zoom, // starting zoom
     });
 
+    this.readFromLocalStorage();
     const markerHtml = document.createElement('div');
     markerHtml.innerHTML = 'Mi marcador';
     // y lo mando en el constructor del objeto {element:markerHTML}
     const marker = new Marker({ color: 'green' }).setLngLat(this.currentLngLat).addTo(this.map);
   }
 
-  createMarker(){
-    if(!this.map)
+  createMarker() {
+    if (!this.map)
       return;
 
-    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const color = '#xxxxxx'.replace(/x/g, y => (Math.random() * 16 | 0).toString(16));
     const lngLat = this.map.getCenter();
     this.addMarker(lngLat, color);
   }
-  addMarker(lngLat: LngLat, color:string){
-    if(!this.map)
+  addMarker(lngLat: LngLat, color: string) {
+    if (!this.map)
       return;
 
-    const marker = new Marker({ color: color, draggable:true }).setLngLat(lngLat).addTo(this.map);
+    const marker = new Marker({ color: color, draggable: true }).setLngLat(lngLat).addTo(this.map);
+
+    this.markers.push(
+      { color, marker } // no hace falta hacer color:color 
+    );
+
+    this.saveToLocalStorage();
+
+    marker.on('dragend', ()=>{
+      console.log(marker.getLngLat());
+      this.saveToLocalStorage(); // puedo hacer esto pues son valores por Referencia
+    })
+  }
+  deleteMarker(i: number) {
+    this.markers[i].marker.remove();
+    this.markers.splice(i, 1);
+  }
+
+  flyTo(marker: Marker) {
+    this.map?.flyTo({
+      zoom: 15,
+      center: marker.getLngLat()
+    })
+  }
+
+  saveToLocalStorage() {
+    // console.log(this.markers);
+    const PlainMarkers: PlainMarker[] = this.markers.map(({ color, marker }) => {
+      return {
+        color,
+        lngLat: marker.getLngLat().toArray()
+      }
+    });
+
+    localStorage.setItem('plainMarkers', JSON.stringify(PlainMarkers));
+  }
+
+  readFromLocalStorage() {
+    const plainMarkersString = localStorage.getItem('plainMarkers') ?? '[]';
+    const plainMarkers: PlainMarker[] = JSON.parse(plainMarkersString); // inseguro , revisar
+
+    plainMarkers.forEach( ({color, lngLat})=>{
+      const [lng, lat] = lngLat;
+      const coords = new LngLat(lng, lat);
+
+      this.addMarker(coords, color);
+    })
   }
 }
