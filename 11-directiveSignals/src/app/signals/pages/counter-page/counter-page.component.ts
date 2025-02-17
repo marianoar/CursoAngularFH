a2f0563ba95776b13397fe6f5c5a8abd3e7aebc4
Mariano <arias.mariano.ar@gmail.com>
2025-02-17 10:04:13 -0300

    signals - counter

--- 11-directiveSignals/src/app/signals/pages/counter-page/counter-page.component.ts
+++ 11-directiveSignals/src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+
+//puedo tener una señal asi suelta fuera del componente
+const name = signal('Mariano'); 
 
 @Component({
   templateUrl: './counter-page.component.html',
@@ -6,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class CounterPageComponent {
 
+  public counter = signal(10);
+
+  public squareCounter = computed(()=> this.counter() * this.counter()); // solo lectura
+
+  constructor(){
+    console.log(name);
+  }
+
+  increaseBy(value: number) {
+    this.counter.update(current => current - value);
+  }
+  decreaseBy(value: number){
+    this.counter.update(c => c + value);
+  }
 }
