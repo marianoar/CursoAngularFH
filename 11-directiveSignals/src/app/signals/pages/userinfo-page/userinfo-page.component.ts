d4739ff2417ef45bca413c1d910f7ad387315416
Mariano <arias.mariano.ar@gmail.com>
2025-02-18 11:51:25 -0300

    Signals computed properties and errors manager

--- 11-directiveSignals/src/app/signals/pages/userinfo-page/userinfo-page.component.ts
+++ 11-directiveSignals/src/app/signals/pages/userinfo-page/userinfo-page.component.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { UsersServiceService } from '../../services/usersService.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './userinfo-page.component.html',
   styleUrl: './userinfo-page.component.css'
 })
-export class UserinfoPageComponent {
+export class UserinfoPageComponent implements OnInit {
+  
+  private usersService = inject(UsersServiceService);
+  
+  public userId = signal(1);
+  
+  public currentUser = signal<User | undefined>(undefined);
+  
+  public userWasFound = signal(true);
+
+  public fullName = computed<string>(()=>{
+    if(!this.currentUser())
+      return 'User not found';
+    return `${this.currentUser()?.last_name}, ${this.currentUser()?.first_name}`;
+  })
+  
+  ngOnInit(): void {
+    this.loadUser(this.userId())
+  }
+
+  loadUser(id:number){
+    if(id<=0)
+      return;
+    this.currentUser.set(undefined); // para que visualmente se vea mas dinamico, sin el delay de la llamada
+    this.userId.set(id);
+     this.usersService.getUserById(id).subscribe(
+      //user=> {
+    //   console.log({user});
+    //   this.currentUser.set(user);
+    {
+      next: (user)=>{
+      this.currentUser.set(user);
+      this.userWasFound.set(true);
+      },
+      error: ()=>{
+        this.userWasFound.set(false),
+        this.currentUser.set(undefined)
+      }
+    })
+  }
 
 }
