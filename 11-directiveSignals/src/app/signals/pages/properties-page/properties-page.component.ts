c3dbaa301368284a8d8628dee34c99ad368f2369
Mariano <arias.mariano.ar@gmail.com>
2025-02-20 11:14:03 -0300

    end Section 24: Signals effects

--- 11-directiveSignals/src/app/signals/pages/properties-page/properties-page.component.ts
+++ 11-directiveSignals/src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,22 +1,46 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, effect, OnDestroy, OnInit, signal } from '@angular/core';
 import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './properties-page.component.html',
   styleUrl: './properties-page.component.css'
 })
-export class PropertiesPageComponent {
+export class PropertiesPageComponent implements OnInit, OnDestroy {
 
-  public user = signal<User>({"id": 2,
-        email: "janet.weaver@reqres.in",
-        first_name: "Janet",
-        last_name: "Weaver",
-        avatar: "https://reqres.in/img/faces/2-image.jpg"});
 
-  public fullName = computed( ()=> `${this.user().first_name} ${this.user().last_name}`);
+  ngOnInit(): void {
+    setInterval(()=>{
+      this.counter.update(current => current + 1);
+    }, 1000)
+  }
+
+  ngOnDestroy(): void {
+    // this.userChangedEffect.destroy();
+  }
+
+  public user = signal<User>({
+    "id": 2,
+    email: "janet.weaver@reqres.in",
+    first_name: "Janet",
+    last_name: "Weaver",
+    avatar: "https://reqres.in/img/faces/2-image.jpg"
+  });
+
+  public fullName = computed(() => `${this.user().first_name} ${this.user().last_name}`);
+
+  // Para que el effect tenga el comportamiento deseado se debe de evitar la mutación de los objetos 
+  // en las signal que son dependencias, y regresar una nueva referencia del objeto.
+  // por eso en el switch aplico un structuredClone en el return del update, 
+  // supongo que al hacer eso, cambia la referencia del objeto, y angular trigerea el effecto
+
+  public userChangedEffect = effect(() => {
+    console.log( `effect ${this.user().first_name} - ${this.counter()}`);
+  })
+
+  public counter = signal(10);
 
   onFieldUpdated(field: keyof User, value: string) {
-    console.log(field + " " + value);
+    // console.log(field + " " + value);
 
     //esto seria inseguro pues si no existe la propertie la generaria
     // this.user.set({
@@ -24,17 +48,22 @@ export class PropertiesPageComponent {
     //   [field]:value,
     // })
 
-    this.user.update( current => {
+    this.user.update(current => {
 
-      switch (field){
+      switch (field) {
         case 'email':
-          current.email=value;
+          current.email = value;
           break;
-          case 'first_name':
-            current.first_name = value;
-          // y asi con todas las propiedades del objeto
+        case 'first_name':
+          current.first_name = value;
+          break;
+        // y asi con todas las propiedades del objeto
       }
-      return current;
+      return structuredClone(current);
     })
+  }
+
+  increaseBy(value: number) {
+    this.counter.update(current => current + value);
   }
 }
